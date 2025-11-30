1189f6afdce71da183cf8e71d752ec1e511e097a
Aloxiy <96200749+Aloxiy@users.noreply.github.com>
2025-11-30 19:53:42 +0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,28 +1,9 @@
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
+let users = [];
+let sessions = [];
+let messages = [];
 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
-export default {
+module.exports = {
   users,
-  messages,
+  sessions,
+  messages
 };
